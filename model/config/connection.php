00f85eb50131194a7f855499c850ad3fa3e014ba
eodaneze <ezealiokechukwu1999@gmail.com>
2025-08-10 20:27:56 +0100

    .env file configuration

--- model/config/connection.php
+++ model/config/connection.php
@@ -1,14 +1,27 @@
 <?php 
 
-  $hostName = 'localhost';
-  $userName = 'root';
-  $password = '';
-  $dbName = 'blog_app';
+   require_once('./config.php');
+
+   try{
+
+       loadEnv();
+
+        $hostName = getenv('DB_HOST');
+        $userName = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
+        $dbName = getenv('DB_NAME');
 
 
-  $conn = mysqli_connect($hostName, $userName, $password, $dbName);
+        $conn = mysqli_connect($hostName, $userName, $password, $dbName);
 
-  if(!$conn){
-     echo "Failed to connect to db";
-  }
+        if(!$conn){
+            echo "Failed to connect to db: " . mysqli_connect_error();
+        }else{
+             echo "Db connection was successful";
+        }
+
+   }catch(Exception $e){
+       die("Error loading environment variables: ". $e->getMessage());
+   }
+ 
 ?>
